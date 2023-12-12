64dbb124666b079e2e4e59e5d70c7a5d7755ff36
Mostafa <jami_789@yahoo.com>
2023-12-12 13:02:05 +0100

    no change

--- index.php
+++ index.php
@@ -23,7 +23,9 @@
 
 <body>
 
-    
+    <div style="background-color: #835D2C;">
+    <?php var_dump($value); ?>
+    </div>
 
     <!-- // BOOTSTRAP -->
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
