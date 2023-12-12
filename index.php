35652d1e88bb9f7e08446ccee4d23ee0f9e61c52
AboutSelphy <AboutSelphy@gmail.com>
2023-12-12 10:36:45 +0100

    index

--- index.php
+++ index.php
@@ -1,3 +1,8 @@
+<?php
+// require_once("./script/db_connection.php");
+
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 
@@ -6,20 +11,21 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Animal Shelter</title>
 
-    // BOOTSTRAP
+    <!-- // BOOTSTRAP -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
 
-    // OWN STYLING
+    <!-- // OWN STYLING -->
     <link rel="stylesheet" href="./resources/style/global.css">
+
+    <!-- Icons FA -->
+    <script src="https://kit.fontawesome.com/96fa54072b.js" crossorigin="anonymous"></script>
 </head>
 
 <body>
 
+    
 
-    // ICONS
-    <script src="https://kit.fontawesome.com/96fa54072b.js" crossorigin="anonymous"></script>
-
-    // BOOTSTRAP
+    <!-- // BOOTSTRAP -->
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
 </body>
 
