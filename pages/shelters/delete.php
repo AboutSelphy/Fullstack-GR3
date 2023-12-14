ceaa30175bf56fe44287da41bc2802e08d4e8232
Svenja Schulmeister <schulmeister.svenja@gmail.com>
2023-12-14 13:14:59 +0100

    fixed the delete_shelters function

--- pages/shelters/delete.php
+++ pages/shelters/delete.php
@@ -2,7 +2,7 @@
 require_once("./../../script/db_connection.php");
 
 // $id =$_GET["id"]; --> will get un-commented once there is an ID in the URL
-$id = 10; // delete this line once everything established
+$id = 12; // delete this line once everything established
 $stmt = $db->prepare("SELECT * FROM shelters WHERE id = $id");
 $stmt->execute();
 $result = $stmt->fetchAll(PDO::FETCH_ASSOC);
@@ -12,17 +12,14 @@ if ($row["image"] != "default.jpg") {
     unlink("../../resources/img/shelters/$row[image]");
 }
 
-$delete = "DELETE FROM shelters WHERE id = $id";
-
-if (mysqli_query($connect, $delete)) {
-    header("Location: ../user/dashboard.php");
-} else {
-    echo "<div class='alert alert-danger' role='alert'>Oh no, something must have gone wrong :(</div>";
-    header("refresh: 3; url= /code-factory/codereview-4/BE20-CR4-SvenjaSchulmeister/animal/home.php");
+try {
+    $delete = "DELETE FROM shelters WHERE id = $id";
+    $stmt = $db->prepare($delete);
+    $stmt->execute();
+    header("Location: ./shelters.php");
+} catch (PDOException $e) {
+    echo "Error: " . $e->getMessage(); // This will display the error message
 }
-
-$stmt = $db->prepare($delete);
-$stmt->execute();
 
 // Close database connection
 $db = NULL;
