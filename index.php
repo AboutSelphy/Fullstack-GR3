a5eb9b30239a00c2d9f61f2ee27bf72c98ce48e9
AboutSelphy <AboutSelphy@gmail.com>
2023-12-19 15:37:55 +0100

    Index Counter

--- index.php
+++ index.php
@@ -1,5 +1,31 @@
 <?php
 require_once("config.php");
+require_once("./script/db_connection.php");
+
+
+// Get all Animals from the corresponding table and display
+$stmtAnimal = $db->prepare("SELECT * FROM `animals`");
+$stmtAnimal->execute();
+$resultAnimal = $stmtAnimal->fetchAll(PDO::FETCH_ASSOC);
+
+// Count the number of animals
+$numAnimals = $stmtAnimal->rowCount();
+
+// Get all Animals from the corresponding table and display
+$stmtShelter = $db->prepare("SELECT * FROM `shelters`");
+$stmtShelter->execute();
+$resultShelter = $stmtShelter->fetchAll(PDO::FETCH_ASSOC);
+
+// Count the number of animals
+$numShelters = $stmtShelter->rowCount();
+
+// Get all Animals from the corresponding table and display
+$stmtUser = $db->prepare("SELECT * FROM `users`");
+$stmtUser->execute();
+$resultshelter = $stmtUser->fetchAll(PDO::FETCH_ASSOC);
+
+// Count the number of animals
+$numUsers = $stmtUser->rowCount();
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -28,17 +54,17 @@ require_once("config.php");
           <div class="carousel-item active c-item">
             <img src="./resources/img/animals/default.jpg" class="d-block w-100 c-img" alt="Slide 1">
             <div class="carousel-caption top-50 translate-middle-y text-center">
-              <p class="mt-5 fs-3 text-uppercase">Discover the hidden world</p>
-              <h1 class="display-1 fw-bolder text-capitalize">The Aurora Tours</h1>
-              <button class="btn btn-cta px-4 py-2 fs-5 mt-5">Book a tour</button>
+              <p class="mt-5 fs-3 text-uppercase">Discover the hidden world:</p>
+              <h1 class="display-1 fw-bolder text-capitalize">Meet Our Animal Ambassadors: Stories of Resilience and Hop</h1>
+              <a href="./pages/animals/animals.php"> <button class="btn btn-cta px-4 py-2 fs-5 mt-5">Our Animals</button> </a>
             </div>
           </div>
           <div class="carousel-item c-item">
             <img src="./resources/img/shelters/default.jpg" class="d-block w-100 c-img" alt="Slide 2">
             <div class="carousel-caption top-50 translate-middle-y text-center">
-              <p class="text-uppercase fs-3 mt-5">The season has arrived</p>
-              <p class="display-1 fw-bolder text-capitalize">3 available tours</p>
-              <button class="btn btn-default px-4 py-2 fs-5 mt-5" data-bs-toggle="modal" data-bs-target="#booking-modal">Book a tour</button>
+              <p class="text-uppercase fs-3 mt-5">Empowering Lives: </p>
+              <p class="display-1 fw-bolder text-capitalize">Discover the Heart of Our Shelter's Mission</p>
+              <a href="./pages/shelters/shelters.php"> <button class="btn btn-default px-4 py-2 fs-5 mt-5" data-bs-toggle="modal" data-bs-target="#booking-modal">Our Sheltersr</button> </a>
             </div>
           </div>
         </div>
@@ -59,7 +85,11 @@ require_once("config.php");
         <div class="row justify-content-center align-items-center">
           <div class="col col-lg-8 col-md-6 col-sx-12">
             <h2>Who we are!</h2>
-            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Pariatur, officiis molestiae iusto suscipit soluta delectus saepe sed minus sint incidunt labore fugit neque explicabo sit. Dolorum quo ut libero consectetur.</p>
+            <p>Welcome to [Wildlife Shelter Name], where dedication meets compassion in our commitment to safeguarding the lives of wild animals. As a refuge for those in need, we strive to rescue, rehabilitate, and release these incredible beings back into their natural habitats.
+
+At [Wildlife Shelter Name], we are more than a shelter; we are a community of passionate individuals devoted to preserving the welfare of wild creatures. Our team works tirelessly to provide a safe haven for animals facing adversity, ensuring they have a chance to thrive in their native environments.
+
+Join us in our mission to protect and nurture the incredible wildlife that graces our planet. At [Wildlife Shelter Name], every contribution, no matter how small, helps build a future where wild animals can flourish in harmony with nature. Welcome to a haven where hearts and habitats unite for a brighter and wilder tomorrow.</p>
             <hr>
             <ul class="nav justify-content-center list-unstyled d-flex">
               <li class="ms-3">
@@ -104,28 +134,28 @@ require_once("config.php");
           <div class="four col-md-3">
             <div class="counter-box ">
               <i class="fa fa-thumbs-o-up"></i>
-              <span class="counter">2147</span>
+              <span class="counter">420</span>
               <p>Happy Customers</p>
             </div>
           </div>
           <div class="four col-md-3">
             <div class="counter-box">
               <i class="fa fa-group "></i>
-              <span class="counter">3275</span>
+              <span class="counter"><?php echo $numUsers; ?></span>
               <p>Registered Members</p>
             </div>
           </div>
           <div class="four col-md-3">
             <div class="counter-box">
               <i class="fa-solid fa-otter"></i>
-              <span class="counter">289</span>
+              <span class="counter"><?php echo $numAnimals; ?></span>
               <p>Available Adoption</p>
             </div>
           </div>
           <div class="four col-md-3">
             <div class="counter-box">
               <i class="fa fa-user"></i>
-              <span class="counter">1563</span>
+              <span class="counter"><?php echo $numShelters; ?></span>
               <p>Registered Shelters</p>
             </div>
           </div>
