788a8c3615ece1f21083772b8f4b63eae0fd8bc0
Svenja Schulmeister <schulmeister.svenja@gmail.com>
2023-12-19 15:20:50 +0100

    application form backend

--- pages/shelters/application.php
+++ pages/shelters/application.php
@@ -1,74 +1,107 @@
 <?php
-require_once("./../../script/db_connection.php");
 //config for global constants
 require_once("../../config.php");
-include("./../../script/loginGate.php");
+
+require_once('../../script/globals.php');
+require_once('../../script/loginGate.php');
+require_once('../../script/getUserIDWithCookieID.php');
+
+include("./../../script/input_validation.php");
+
+// if ($role !== 'unset') {
+//     if ($role === 'shelter') {
+//         header("Location: " . ROOT_PATH . "pages/sh_dashboard.php");
+//         exit();
+//     }
+//     if ($role === 'admin') {
+//         header("Location: " . ROOT_PATH . "pages/dashboard.php");
+//         exit();
+//     }
+// }
+
+$loc = "./";
 
 if ($role !== 'unset') {
     if ($role === 'shelter') {
         header("Location: " . ROOT_PATH . "pages/sh_dashboard.php");
         exit();
     }
-    if ($role === 'admin') {
-        header("Location: " . ROOT_PATH . "pages/dashboard.php");
-        exit();
-    }
 }
 
-include("./../../script/input_validation.php");
+$cookieID = $_COOKIE['sessionID'];
+try {
+    $stmt = $db->prepare("SELECT users.id
+                            FROM `login`
+                            INNER JOIN `users`
+                            ON login.userID = users.id
+                            WHERE login.sessionID = :cookieID");
+    $stmt->bindParam(':cookieID', $cookieID);
+    $stmt->execute();
+    $userData = $stmt->fetch();
+} catch (PDOException $e) {
+    echo "Error: " . $e->getMessage();
+}
 
 // Preparing validation/error messages
 $error = false;
-$name = $animals = $zip = $philosophy = "";
-$nameError = $animalsError = $zipError = $philosophyError = "";
+$zip = $city = $country = $zipError = $cityError = $countryError = "";
 
 // Clean, validate & store data from input form into variable
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    $zip = clean($_POST['zip']);
+    $city = clean($_POST['city']);
+    $country = clean($_POST['country']);
 
-    $name = clean($_POST['name']);
-    $animals = clean($_POST['animals']);
-    $zip = $_POST['zip'] != 0 ? $_POST['zip'] : 1;
-    $image = fileUpload($_FILES['image'], "shelter");
-    $philosophy = clean($_POST['philosophy']);
-
-    if (validate($name, $nameError, 255)[0] == true) {
+    if (empty($zip)) {
         $error = true;
-        $nameError = validate($name, $nameError, 255)[1];
+        $zipError = "Please insert some data here!";
     }
 
-    if (empty($animals)) {
+    if (validate($city, $cityError, 50)[0] == true) {
         $error = true;
-        $animalsError = "Please insert some data here!";
+        $cityError = validate($city, $cityError, 50)[1];
     }
 
-    if ($zip == "0") {
+    if (validate($country, $countryError, 50)[0] == true) {
         $error = true;
-        $zipError = "Please select one option!";
-    }
-
-    if (strlen($philosophy) > 1000) {
-        $error = true;
-        $philosophyError = "Your input must not have more than 1000 characters!";
+        $countryError = validate($country, $countryError, 50)[1];
     }
 
     $data = [
-        'name' => $name,
-        'animals' => $animals,
-        'image' => $image[0],
-        'philosophy' => $philosophy,
-        'zip' => $zip
+        'zip' => $zip,
+        'city' => $city,
+        'country' => $country
     ];
 
-    // Prepare and execute SQL insertion
-    if (!$error) {
-        $sql = "INSERT INTO `shelters`(`shelter_name`, `animals`, `image`, `philosophy`, `fk_zip`) VALUES (:name, :animals, :image, :philosophy, :zip)";
-        $stmt = $db->prepare($sql);
-        $stmt->execute($data);
+    // Fetch already exisiting addresses from the table `zip`
+    try {
+        $addressList = $db->prepare("SELECT * FROM `zip` WHERE zip= :zip");
+        $addressList->bindParam(':zip', $zip, PDO::PARAM_INT);
+        $addressList->execute();
+        $addresses = $addressList->fetchAll(PDO::FETCH_ASSOC);
+    } catch (PDOException $e) {
+        echo "Error: " . $e->getMessage();
+    }
 
-        echo 'db entry created';
-        header('Location: ./shelters.php');
+    // Checks if the submitted address is already existing, else it gets pushed into the table
+    if (is_array($addresses) && count($addresses)) {
+        echo "Address exists already!";
     } else {
-        echo 'oh no, a problem';
+        try {
+            $stmt = $db->prepare("INSERT INTO `zip`(`zip`, `city`, `country`) VALUES (:zip, :city, :country)");
+            $stmt->execute($data);
+        } catch (PDOException $e) {
+            echo "Error: " . $e->getMessage();
+        }
+    }
+
+    // The boolean shelterRequest gets changed to true
+    try {
+        $request = $db->prepare("UPDATE `users`
+                            SET `shelterRequest`=1 WHERE id = $userData[id]");
+        $request->execute();
+    } catch (PDOException $e) {
+        echo "Error: " . $e->getMessage();
     }
 }
 
@@ -83,7 +116,7 @@ $db = NULL;
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Animal Shelter</title>
+    <title>Shelter Application</title>
 
     <!-- // BOOTSTRAP -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
@@ -100,31 +133,26 @@ $db = NULL;
 
     <div class="container">
         <form method="post" enctype="multipart/form-data">
-            <div class="form-group">
-
-            </div>
-
-            <div class="form-group">
-                <label for="age">Animals:</label>
-                <input type="number" min="1" name="animals" id="animals" class="form-control" value="<?= $animals ?? "" ?>">
-                <span><?= $animalsError ?></span>
-            </div>
 
             <div class="form-group">
                 <label for="zip" class="form-label">ZIP:</label>
-                <input type="number" min="1" class="form-control" id="zip">
+                <input type="number" min="1" class="form-control" name="zip" id="zip" value="<?= $zip ?? "" ?>">
+                <span><?= $zipError ?></span>
             </div>
 
             <div class="form-group">
                 <label for="city" class="form-label">City:</label>
-                <input type="text" class="form-control" id="city">
+                <input type="text" class="form-control" id="city" name="city" value="<?= $city ?? "" ?>">
+                <span><?= $cityError ?></span>
             </div>
 
             <div class="form-group">
                 <label for="country" class="form-label">Country:</label>
-                <input type="text" class="form-control" id="country">
+                <input type="text" class="form-control" id="country" name="country" value="<?= $country ?? "" ?>">
+                <span><?= $countryError ?></span>
             </div>
 
+            <!--
             <div class="form-group">
                 <label class="form-check-label" for="profit">Non-Profit:</label>
                 <input class="form-check-input" type="checkbox" value="0" id="profit" name="profit">
@@ -135,6 +163,7 @@ $db = NULL;
                 <textarea name="philosophy" id="philosophy" class="form-control"><?= $philosophy ?? "" ?></textarea>
                 <span><?= $philosophyError ?></span>
             </div>
+            -->
 
             <button type="submit" value="Submit" class="btn btn-default">Submit</button>
 
