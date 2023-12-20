2e0664850c5022ac7716e9a97d8bfbe97d3f4ce8
Svenja Schulmeister <schulmeister.svenja@gmail.com>
2023-12-20 13:33:07 +0100

    added images / register.php

--- pages/users/register.php
+++ pages/users/register.php
@@ -12,25 +12,25 @@ require_once('./../../script/isLoggedIn.php');
 
 
 //check if client has entry in db table login
-    $role = isLoggedIn($db)[1];
+$role = isLoggedIn($db)[1];
 
-    // if $role is set redirect to loginpage that handles the redirect to right dashboard
-    if($role !== 'unset' && $role === 'admin'){
-    }elseif($role !== 'unset'){
-        header("Location: " . ROOT_PATH . "pages/login/login.php");
-        exit();
-    }
+// if $role is set redirect to loginpage that handles the redirect to right dashboard
+if ($role !== 'unset' && $role === 'admin') {
+} elseif ($role !== 'unset') {
+    header("Location: " . ROOT_PATH . "pages/login/login.php");
+    exit();
+}
 
 
 // Getting ZIP (foreign key) options
-$locations = "";
-$stmt = $db->prepare("SELECT * FROM `zip`");
-$stmt->execute();
-$result = $stmt->fetchAll(PDO::FETCH_ASSOC);
+// $locations = "";
+// $stmt = $db->prepare("SELECT * FROM `zip`");
+// $stmt->execute();
+// $result = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
-foreach ($result as $row) {
-    $locations .= "<option value='{$row['id']}'>{$row['city']}</option>";
-}
+// foreach ($result as $row) {
+//     $locations .= "<option value='{$row['id']}'>{$row['city']}</option>";
+// }
 
 // Getting Shelters (foreign key) options
 // $shelters = "";
@@ -45,16 +45,70 @@ foreach ($result as $row) {
 // Preparing validation/error messages
 $error = false;
 
-$first_nameError = $last_nameError = $addressError = $emailError = $passwordError = $zipError =  "";
+$first_nameError = $last_nameError = $addressError = $emailError = $passwordError = $zipError = $cityError = $countryError = "";
 
-
-// Clean, validate & store data from input form into variable
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 
-    // echo '<pre>';
-    // var_dump($_POST);
-    // echo '</pre>';
+    // Clean, validate & store data from input form into variable
+    $zip = intval(clean($_POST['zip']));
+    $city = clean($_POST['city']);
+    $country = clean($_POST['country']);
 
+    if (empty($zip)) {
+        $error = true;
+        $zipError = "Please insert some data here!";
+    }
+
+    if (validate($city, $cityError, 50)[0] == true) {
+        $error = true;
+        $cityError = validate($city, $cityError, 50)[1];
+    }
+
+    if (validate($country, $countryError, 50)[0] == true) {
+        $error = true;
+        $countryError = validate($country, $countryError, 50)[1];
+    }
+
+    $location = [
+        'zip' => $zip,
+        'city' => $city,
+        'country' => $country
+    ];
+
+    echo '<pre style="padding: 10px; background-color: black; color: white;">';
+    var_dump($location);
+    echo '</pre>';
+
+    // Fetch already exisiting addresses from the table `zip`
+    try {
+        $addressList = $db->prepare("SELECT * FROM `zip` WHERE zip= :zip");
+        $addressList->bindParam(':zip', $zip, PDO::PARAM_INT);
+        $addressList->execute();
+        $addresses = $addressList->fetchAll(PDO::FETCH_ASSOC);
+    } catch (PDOException $e) {
+        echo "Error: " . $e->getMessage();
+    }
+
+    echo '<pre style="padding: 10px; background-color: black; color: white;">';
+    var_dump($addresses);
+    echo '</pre>';
+    exit();
+
+    // Checks if the submitted address is already existing, else it gets pushed into the table
+    if (is_array($addresses) && count($addresses)) {
+        echo "Address exists already!";
+    } else {
+        try {
+            $stmt = $db->prepare("INSERT INTO `zip`(`zip`, `city`, `country`) VALUES (:zip, :city, :country)");
+            $stmt->execute($location);
+        } catch (PDOException $e) {
+            echo "Error: " . $e->getMessage();
+        }
+    }
+
+
+
+    // Clean, validate & store data from input form into variable
     $first_name = clean($_POST['first_name']);
     $last_name = clean($_POST['last_name']);
     $email = clean($_POST['email']);
@@ -62,7 +116,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $password = clean($_POST['password']);
     // $accountType = clean($_POST['accountType']);
 
-    $zip = $_POST['zip'] != 0 ? $_POST["zip"] : 0;
+    // $zip = $_POST['zip'] != 0 ? $_POST["zip"] : 0;
     // $shelter = $_POST['shelter'] != 0 ? $_POST["shelter"] : 0;
 
     $image = fileUpload($_FILES["image"], "user");
@@ -83,24 +137,26 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
         $addressError = validate($address, $addressError, 255)[1];
     }
     //VAL $email
-    if(!filter_var($email,FILTER_VALIDATE_EMAIL))
-    {
+    if (validate($email, $emailError, 255)[0] == true) {
         $error = true;
-        $emailError = "Please enter a valid Email adress";
-    }
-    else
-    {
-        if($role === 'unset'){ // if no role is set
+        $emailError = validate($email, $emailError, 255)[1];
+
+        if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+            $error = true;
+            $emailError = "Please enter a valid email adress";
+        }
+    } else {
+        if ($role === 'unset') { // if no role is set
             $stmt = $db->prepare("SELECT email FROM `users` WHERE email = :email");
             $stmt->bindParam(':email', $email);
             $stmt->execute();
             $emailResult = $stmt->fetch();
 
-                if (is_array($emailResult) && count($emailResult) != 0 ){
-                    $errorStatus = true ;
-                    $emailError = "Provided Email is already in use" ;
-                }
+            if (is_array($emailResult) && count($emailResult) != 0) {
+                $errorStatus = true;
+                $emailError = "Provided email is already in use";
             }
+        }
     }
     //VAL password
     if (validate($password, $passwordError, 255)[0] == true) {
@@ -109,10 +165,10 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     }
 
     //VAL zip
-    if ($zip == "0") {
-        $error = true;
-        $zipError = "Please select one option!";
-    }
+    // if ($zip == "0") {
+    //     $error = true;
+    //     $zipError = "Please select one option!";
+    // }
 
     //VAL shelter
     // if ($shelter == "0") {
@@ -125,29 +181,26 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
         'last_name' => $last_name,
         'address' => $address,
         'email' => $email,
-        'password' => hash('sha256',$password),
+        'password' => hash('sha256', $password),
         'profile' => $image[0],
-        'zip' => $zip,
+        // 'zip' => $zip,
         // 'shelter' => $shelter,
         // 'status' => $accountType
     ];
 
     // Prepare and execute SQL insertion
-    if($error === false){
+    if ($error === false) {
         try {
-            $sql = "INSERT INTO `users`(`first_name`, `last_name`, `address`, `email`, `password`,`profile`,`fk_zip`) VALUES (:first_name, :last_name, :address, :email, :password, :profile, :zip)";
+            $sql = "INSERT INTO `users`(`first_name`, `last_name`, `address`, `email`, `password`,`profile`,`fk_zip`) VALUES (:first_name, :last_name, :address, :email, :password, :profile)";
             $stmt = $db->prepare($sql);
             $stmt->execute($data);
-    
+
             echo 'successfully registered :)';
             header("refresh:2;url=" . ROOT_PATH . "pages/login/login.php");
-
-    
         } catch (PDOException $e) {
             // echo "Error: " . $e->getMessage(); 
         }
     }
-
 }
 
 // Close database connection
@@ -176,69 +229,78 @@ $db = NULL;
 <body>
     <?php require_once("./../../components/navbar.php") ?>
 
-    <div class="container">
-        <form method="post" enctype="multipart/form-data">
-            <div class="form-group">
-                <label for="first_name">First Name:</label>
-                <input autocomplete="on" type="text" name="first_name" id="first_name" class="form-control" value="<?= $first_name ?? "" ?>">
-                <span style="color:red;"><?= $first_nameError ?></span>
+    <div class="container my-5">
+        <h2 class="text-center uppercase mb-5">Register yourself</h2>
+        <form class="row g-3" method="post" enctype="multipart/form-data">
+            <div class="col-md-6 px-2">
+                <div class="form-group">
+                    <label for="first_name">First Name:</label>
+                    <input autocomplete="on" type="text" name="first_name" id="first_name" class="form-control" value="<?= $first_name ?? "" ?>">
+                    <span class="error"><?= $first_nameError ?></span>
+                </div>
             </div>
-            <div class="form-group">
-                <label for="last_name">Last Name:</label>
-                <input autocomplete="on" type="text" name="last_name" id="last_name" class="form-control" value="<?= $last_name ?? "" ?>">
-                <span style="color:red;"><?= $last_nameError ?></span>
+            <div class="col-md-6 px-2">
+                <div class="form-group">
+                    <label for="last_name">Last Name:</label>
+                    <input autocomplete="on" type="text" name="last_name" id="last_name" class="form-control" value="<?= $last_name ?? "" ?>">
+                    <span class="error"><?= $last_nameError ?></span>
+                </div>
             </div>
-            <div class="form-group">
-                <label for="address">Address:</label>
-                <input autocomplete="on" type="text" name="address" id="address" class="form-control" value="<?= $address ?? "" ?>">
-                <span style="color:red;"><?= $addressError ?></span>
+            <div class="col-md-6 px-2">
+                <div class="form-group">
+                    <label for="email">Email:</label>
+                    <input autocomplete="on" type="email" name="email" id="email" class="form-control" value="<?= $email ?? "" ?>">
+                    <span class="error"><?= $emailError ?></span>
+                </div>
             </div>
-            <div class="form-group">
-                <label for="email">Email:</label>
-                <input autocomplete="on" type="email" name="email" id="email" class="form-control" value="<?= $email ?? "" ?>">
-                <span style="color:red;"><?= $emailError ?></span>
+            <div class="col-md-6 px-2">
+                <div class="form-group">
+                    <label for="password">Password:</label>
+                    <input autocomplete="on" type="password" name="password" id="password" class="form-control" value="<?= $password ?? "" ?>">
+                    <span class="error"><?= $passwordError ?></span>
+                </div>
             </div>
-            <div class="form-group">
-                <label for="password">Password:</label>
-                <input autocomplete="on" type="password" name="password" id="password" class="form-control" value="<?= $password ?? "" ?>">
-                <span style="color:red;"><?= $passwordError ?></span>
+            <div class="col-md-12 px-2">
+                <div class="form-group">
+                    <label for="image">Profile Picture:</label>
+                    <input type="file" name="image" id="image" class="form-control">
+                </div>
             </div>
-            <!-- <div class="form-group">
-                <label for="accountType">Account Type:</label>
-                <select name="accountType" id="accountType" class="form-control">
-                    <option value="user" <?= $_SERVER['REQUEST_METHOD'] === 'POST' && $accountType == 'user' ? 'selected' : ''?>>User</option>
-                    <option value="shelter" <?= $_SERVER['REQUEST_METHOD'] === 'POST' && $accountType == 'shelter' ? 'selected' : ''?>>Shelter</option>
-                </select>
-            </div> -->
-
-            <div class="form-group">
-                <label for="zip">ZIP:</label>
-                <select name="zip" id="zip" class="form-control">
-                    <option value="0">Please choose...</option>
-                    <?= $locations ?>
-                </select>
-                <span style="color:red;"><?= $zipError ?></span>
+            <div class="col-md-12 px-2">
+                <div class="form-group">
+                    <label for="address">Address:</label>
+                    <input autocomplete="on" type="text" name="address" id="address" class="form-control" value="<?= $address ?? "" ?>">
+                    <span class="error"><?= $addressError ?></span>
+                </div>
             </div>
-
-            <!-- <div class="form-group">
-                <label for="shelter">Shelter:</label>
-                <select name="shelter" id="shelter" class="form-control">
-                    <option value="0">Please choose...</option>
-                </select>
-                <span style="color:red;"></span>
-            </div> -->
-
-            <div class="form-group">
-                <label for="image">ProfilePicture:</label>
-                <input type="file" name="image" id="image" class="form-control">
+            <div class="col-md-4 px-2">
+                <div class="form-group">
+                    <label for="zip" class="form-label">ZIP:</label>
+                    <input type="number" min="1" class="form-control" name="zip" id="zip" value="<?= $zip ?? "" ?>">
+                    <span class="error"><?= $zipError ?></span>
+                </div>
             </div>
-
-            <button type="submit" value="Submit" class="btn btn-default">Create Account</button>
-
+            <div class="col-md-4 px-2">
+                <div class="form-group">
+                    <label for="city" class="form-label">City:</label>
+                    <input type="text" class="form-control" id="city" name="city" value="<?= $city ?? "" ?>">
+                    <span class="error"><?= $cityError ?></span>
+                </div>
+            </div>
+            <div class="col-md-4 px-2">
+                <div class="form-group">
+                    <label for="country" class="form-label">Country:</label>
+                    <input type="text" class="form-control" id="country" name="country" value="<?= $country ?? "" ?>">
+                    <span class="error"><?= $countryError ?></span>
+                </div>
+            </div>
+            <div class="col-md-12 text-center">
+                <button type="submit" value="Submit" class="btn btn-default w-25 mt-5">Create Account</button>
+            </div>
         </form>
     </div>
-    <!-- // BOOTSTRAP -->
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
+
+    <?php require_once("./../../components/footer.php") ?>
 </body>
 
 </html>
